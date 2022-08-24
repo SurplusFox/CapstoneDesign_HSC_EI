656687b883a0187d37b1c43056038d6f1d0d1002
SurplusFox <whaql5000@gmail.com>
2022-08-24 23:46:30 +0900

    ESP 이식 및 기능 수정
    코드를 ESP32에 맞게 다시 수정하였고 몇몇 기능을 추가하고 삭제하였습니다.

--- CapstoneDesign_HSC_EI.ino
+++ CapstoneDesign_HSC_EI.ino
@@ -1,122 +1,217 @@
-#define relayPin 2
-#define trigPin 4
-#define echoPin 6
-#define soundPin 8
-#define distanceLedPin 10
-#define enginePin 13
-#define alcoholPin A5
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <LiquidCrystal_I2C.h>
+
+#define lockRelayPin 12 // lock: LOW, unlock: HIGH
+#define enginePin 13    // on: 0, off: 1
+#define alcoholPin 14
+#define bottonPin 15
+#define redLampRelayPin 16
+#define greenLampRelayPin 17
+
+#define soundPin 18
+
+#define returnDelay 1000
+
+#define testTime 4000
+
+#define count 32
+static double list[count] = {};
+static int last = 0;
 
 static bool pass = false;
+static bool message_FirstPass = true;
+static bool message_FirstEengine = true;
 
-static int list[10] = {};
-static int last = 0;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
   Serial.begin(9600);
 
-  pinMode(relayPin, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(distanceLedPin, OUTPUT);
-  pinMode(enginePin, INPUT);
+  pinMode(lockRelayPin, OUTPUT);
+  pinMode(enginePin, INPUT_PULLUP);
+  pinMode(alcoholPin, INPUT);
+  pinMode(bottonPin, INPUT_PULLUP);
+  pinMode(redLampRelayPin, OUTPUT);
+  pinMode(greenLampRelayPin, OUTPUT);
+  pinMode(soundPin, OUTPUT);
 
-  digitalWrite(relayPin, HIGH);
+  digitalWrite(lockRelayPin, LOW);
+  digitalWrite(redLampRelayPin, HIGH);
+  digitalWrite(greenLampRelayPin, LOW);
 
-  resetListAndLast();
+  init_List();
+
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop()
 {
-  if (digitalRead(enginePin) == HIGH)
+  // debug_State();
+  //====================================================================================================//
+  if (digitalRead(enginePin) == HIGH) // 엔진이 꺼져있는 상태
   {
-    if (pass == true)
+    if (message_FirstEengine == true) // 화면 깜박이는 현상을 방지하기 위해 1회만 출력
     {
-      Serial.println("엔진을 끔 테스트 리셋");
-      pass = false;
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Engine Off");
+      message_FirstEengine = false;
     }
-    Serial.println("엔진이 꺼져있음");
-    digitalWrite(relayPin, HIGH);
-    digitalWrite(distanceLedPin, LOW);
-    delay(1000);
-    return;
-  }
-  if (pass)
-  {
-    Serial.println("테스트 통과함");
-    delay(1000);
-    return;
-  }
-
-  unsigned long duration, distance;
-
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  duration = pulseIn(echoPin, HIGH); // ms
-  distance = duration / 58.2;        // cm
-
-  if (distance <= 10)
-  {
-    digitalWrite(distanceLedPin, HIGH);
-    list[last] = analogRead(alcoholPin);
-    last += 1;
-    Serial.print(". ");
-    if (last == 10)
+    //====================================================================================================//
+    if (pass == true) // 만약 테스트를 통과한 상태면 테스트 결과를 리셋하고 핸들을 잠금
     {
-      Serial.println("");
-      last = 0;
-
-      int result = 0;
-      for (int i = 0; i < 10; i++)
+      pass = false;
+      digitalWrite(lockRelayPin, LOW);
+      digitalWrite(redLampRelayPin, HIGH);
+      digitalWrite(greenLampRelayPin, LOW);
+      lcd.setCursor(0, 1);
+      lcd.print("Test Reset");
+      message_FirstPass = true; // message_FirstPass 초기화
+    }
+    //====================================================================================================//
+    delay(returnDelay);
+    return;
+  }
+  //====================================================================================================//
+  if (message_FirstEengine == false) // message_FirstEengine 초기화 하는 겸 Engine On 메세지도 출력
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Engine On");
+    message_FirstEengine = true;
+    delay(returnDelay * 2);
+  }
+  //====================================================================================================//
+  if (pass == true) // 엔진이 켜져있고 테스트를 이미 통과한 상태면 메세지 출력 후 대기
+  {
+    if (message_FirstPass == true) // 화면 깜박이는 현상을 방지하기 위해 1회만 출력
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Test Passed");
+      message_FirstPass = false;
+    }
+    //====================================================================================================//
+    delay(returnDelay);
+    return;
+  }
+  //====================================================================================================//
+  if (digitalRead(bottonPin) == LOW) // 버튼을 눌러 테스트 시작함
+  {
+    //====================================================================================================//
+    list[last] = analogRead(alcoholPin) / (double)4095; // 0.00 ~ 1.00 //
+    //====================================================================================================//
+    // LCD 최초 1회 초기화 후 테스트 진행도 표시
+    if (last == 0)
+    {
+      lcd.clear();
+    }
+    if (last < 16)
+    {
+      lcd.setCursor(last, 0);
+      lcd.print(".");
+    }
+    else
+    {
+      lcd.setCursor(last - 16, 1);
+      lcd.print(".");
+    }
+    last += 1;
+    //====================================================================================================//
+    if (last != count) // 테스트를 전부 완료하지 못함
+    {
+      delay(testTime / count);
+      return;
+    }
+    //====================================================================================================//
+    else // 테스트를 전부 완료함
+    {
+      double result = 0;
+      for (int i = 0; i < count; i++)
       {
-        Serial.print(i + 1);
-        Serial.print("회차 값 : ");
-        Serial.println(list[i]);
         result += list[i];
       }
-      result = result / 10;
-      if (result > 0)
-      {
-        if (result < 400)
-        {
-          Serial.print("테스트 통과 10회 평균값 : ");
-          Serial.println(result);
-          digitalWrite(relayPin, LOW);
-          digitalWrite(distanceLedPin, LOW);
-          pass = true;
-        }
-        else
-        {
-          Serial.print("??? 동작그만! 10회 평균값 : ");
-          tone(soundPin, 830, 5000);
-          delay(5000);
-          noTone(soundPin);
-          return;
-        }
-      }
-    }
-    delay(400);
-    return;
-  }
-  else
-  {
-    Serial.println("거리가 멀어 테스트 중지");
 
-    digitalWrite(distanceLedPin, LOW);
-    resetListAndLast();
+      result = result / count;
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      //====================================================================================================//
+      if (result < 0.4) // 테스트 통과함
+      {
+        pass = true;
+        digitalWrite(lockRelayPin, HIGH);
+        digitalWrite(redLampRelayPin, LOW);
+        digitalWrite(greenLampRelayPin, HIGH);
+
+        lcd.print("Test Pass");
+      }
+      //====================================================================================================//
+      else // 테스트에 통과하지 못함
+      {
+        tone(soundPin, 800, 2000);
+        lcd.print("Test Failure");
+      }
+      //====================================================================================================//
+      // 테스트 결과 출력
+      lcd.setCursor(0, 1);
+      lcd.print("Result:");
+      lcd.setCursor(7, 1);
+      lcd.print(result * 100);
+      lcd.setCursor(12, 1);
+      lcd.print("%");
+      init_List();
+      delay(returnDelay * 2);
+      //====================================================================================================//
+    }
   }
-  delay(1000);
+  //====================================================================================================//
+  else if (last != 0) // 테스트 도중 중지함
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Stop Testing");
+    init_List();
+  }
+  //====================================================================================================//
+  else // 테스트 대기중
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Wating For Test");
+  }
+  //====================================================================================================//
+  delay(returnDelay);
+  return;
 }
 
-void resetListAndLast()
+void init_List()
 {
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < count; i++)
   {
-    list[i] = -32768;
+    list[i] = -1 * count;
   }
   last = 0;
+}
+
+void debug_State()
+{
+  Serial.print("lockRelayPin : ");
+  Serial.println(digitalRead(lockRelayPin));
+  Serial.print("enginePin : ");
+  Serial.println(digitalRead(enginePin));
+  Serial.print("alcoholPin : ");
+  Serial.println(analogRead(alcoholPin) / (double)4095);
+  Serial.print("bottonPin : ");
+  Serial.println(digitalRead(bottonPin));
+  Serial.print("redLampRelayPin : ");
+  Serial.println(digitalRead(redLampRelayPin));
+  Serial.print("greenLampRelayPin : ");
+  Serial.println(digitalRead(greenLampRelayPin));
+  Serial.print("soundPin : ");
+  Serial.println(digitalRead(soundPin));
+  Serial.println("==================================================");
 }
